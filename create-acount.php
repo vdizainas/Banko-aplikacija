2c12a92f7acb486c62e0864d2685cc762d49f81c
vdizainas <80696811+vdizainas@users.noreply.github.com>
2021-12-17 21:30:00 +0200

    update create acount pg., acounts

--- create-acount.php
+++ create-acount.php
@@ -1,177 +1,85 @@
-<?php 
-$title = 'Naujas sąskaita';
-?>
-
 <!DOCTYPE html>
 <!--[if lte 8]><html class="pre-ie9" lang="en"><![endif]-->
 <!--[if gte IE 9]><!--><html lang="en"><!--<![endif]-->
 <html lang="en">
 <!-- head -->
 <?php
-  include('head.php')
-?>
+  $title = 'Naujas sąskaita';
+  include('head.php');
+  include('header.php');
 
-<!-- header -->
-<?php
-  include('header.php')
+$db = file_get_contents('./db/db.json');
+$db = json_decode($db, true);
+
+if( isset($_POST['kliento']) ) {
+  if(
+    $_POST['kliento']['vardas'] != '' &&
+    $_POST['kliento']['pavarde'] != '' &&
+    $_POST['kliento']['ask'] != '' &&
+    $_POST['kliento']['saskaitos_nr'] != ''
+  ) {
+    $db = file_get_contents('./db/db.json');
+    $db = json_decode($db);
+    
+    $klientas = [$_POST['kliento']];
+
+    if($db) {
+      $klientas = array_merge($db, $klientas);
+    }
+
+    $json = json_encode($klientas);
+    file_put_contents('./db/db.json', $json);
+
+    print_r($db);
+
+  }
+}
+
+
+
 ?>
 
 <body>
   <div class="container">
     <div class="row">
 
-      <div class="col-12">
+      <div class="col-8 m-auto">
+        
             <h1 class="mb-3 fs-3 fw-semibold">Sukurti naują sąskaitą</h1>
-            <form class="needs-validation" novalidate="">
+            <form method="POST" class="needs-validation" novalidate="">
               <div class="row g-3">
                 <div class="col-sm-6">
-                  <label for="firstName" class="form-label">First name</label>
-                  <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
+                  <label for="firstName" class="form-label">Vardas</label>
+                  <input type="text" class="form-control" id="firstName" name="kliento[vardas]" placeholder="" value="" required="">
                   <div class="invalid-feedback">
-                    Valid first name is required.
+                    Šis laukas privalomas
                   </div>
                 </div>
 
                 <div class="col-sm-6">
-                  <label for="lastName" class="form-label">Last name</label>
-                  <input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
+                  <label for="lastName" class="form-label">Pavardė</label>
+                  <input type="text" class="form-control" id="lastName" placeholder="" value="" required="" name="kliento[pavarde]">
                   <div class="invalid-feedback">
-                    Valid last name is required.
+                    Šis laukas privalomas
                   </div>
                 </div>
 
                 <div class="col-12">
-                  <label for="username" class="form-label">Username</label>
-                  <div class="input-group has-validation">
-                    <span class="input-group-text">@</span>
-                    <input type="text" class="form-control" id="username" placeholder="Username" required="">
+                  <label for="ask" class="form-label">Asmens kodas</label>
+                  <input type="number" class="form-control" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"  maxlength="11" name="kliento[ask]" required=""/>
                   <div class="invalid-feedback">
-                      Your username is required.
-                    </div>
+                    Šis laukas privalomas
                   </div>
                 </div>
 
                 <div class="col-12">
-                  <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-                  <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                  <label for="acount-number" class="form-label">Sąskaitos numeris</label>
+                  <input type="number" class="form-control" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxlength="16" for="acount-number" name="kliento[saskaitos_nr]" required=""/>
                   <div class="invalid-feedback">
-                    Please enter a valid email address for shipping updates.
+                    Šis laukas privalomas
                   </div>
                 </div>
-
-                <div class="col-12">
-                  <label for="address" class="form-label">Address</label>
-                  <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
-                  <div class="invalid-feedback">
-                    Please enter your shipping address.
-                  </div>
-                </div>
-
-                <div class="col-12">
-                  <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-                  <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-                </div>
-
-                <div class="col-md-5">
-                  <label for="country" class="form-label">Country</label>
-                  <select class="form-select" id="country" required="">
-                    <option value="">Choose...</option>
-                    <option>United States</option>
-                  </select>
-                  <div class="invalid-feedback">
-                    Please select a valid country.
-                  </div>
-                </div>
-
-                <div class="col-md-4">
-                  <label for="state" class="form-label">State</label>
-                  <select class="form-select" id="state" required="">
-                    <option value="">Choose...</option>
-                    <option>California</option>
-                  </select>
-                  <div class="invalid-feedback">
-                    Please provide a valid state.
-                  </div>
-                </div>
-
-                <div class="col-md-3">
-                  <label for="zip" class="form-label">Zip</label>
-                  <input type="text" class="form-control" id="zip" placeholder="" required="">
-                  <div class="invalid-feedback">
-                    Zip code required.
-                  </div>
-                </div>
-              </div>
-
-              <hr class="my-4">
-
-              <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="same-address">
-                <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-              </div>
-
-              <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="save-info">
-                <label class="form-check-label" for="save-info">Save this information for next time</label>
-              </div>
-
-              <hr class="my-4">
-
-              <h4 class="mb-3">Payment</h4>
-
-              <div class="my-3">
-                <div class="form-check">
-                  <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked="" required="">
-                  <label class="form-check-label" for="credit">Credit card</label>
-                </div>
-                <div class="form-check">
-                  <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required="">
-                  <label class="form-check-label" for="debit">Debit card</label>
-                </div>
-                <div class="form-check">
-                  <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required="">
-                  <label class="form-check-label" for="paypal">PayPal</label>
-                </div>
-              </div>
-
-              <div class="row gy-3">
-                <div class="col-md-6">
-                  <label for="cc-name" class="form-label">Name on card</label>
-                  <input type="text" class="form-control" id="cc-name" placeholder="" required="">
-                  <small class="text-muted">Full name as displayed on card</small>
-                  <div class="invalid-feedback">
-                    Name on card is required
-                  </div>
-                </div>
-
-                <div class="col-md-6">
-                  <label for="cc-number" class="form-label">Credit card number</label>
-                  <input type="text" class="form-control" id="cc-number" placeholder="" required="">
-                  <div class="invalid-feedback">
-                    Credit card number is required
-                  </div>
-                </div>
-
-                <div class="col-md-3">
-                  <label for="cc-expiration" class="form-label">Expiration</label>
-                  <input type="text" class="form-control" id="cc-expiration" placeholder="" required="">
-                  <div class="invalid-feedback">
-                    Expiration date required
-                  </div>
-                </div>
-
-                <div class="col-md-3">
-                  <label for="cc-cvv" class="form-label">CVV</label>
-                  <input type="text" class="form-control" id="cc-cvv" placeholder="" required="">
-                  <div class="invalid-feedback">
-                    Security code required
-                  </div>
-                </div>
-              </div>
-
-              <hr class="my-4">
-
-              <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+              <button class="btn btn-primary btn-lg mt-5" type="submit">Sukurti</button>
             </form>
           </div>
       </div>
@@ -180,6 +88,33 @@ $title = 'Naujas sąskaita';
 
 
 </body>  
+
+<!-- formos validacija -->
+
+<!-- <script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+</script> -->
+
+
 
 <!-- footer -->
 <?php
